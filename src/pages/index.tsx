494d15dceac437e30ac5f828ecb2fafd2d645f05
Pavel Reznikov <pashka.reznikov@gmail.com>
2022-03-05 08:52:05 -0800

    in progress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,52 @@
 import * as React from "react";
 
 import '../styles/main.scss';
+import standWithUkraine from '../images/stand-with-ukraine.png';
 
 // markup
 const IndexPage = () => {
   return (
     <>
-      <section className="hero is-fullheight main-hero">
+      <section className="hero is-halfheight main-hero">
         <div className="hero-body">
           <div className="container has-text-centered">
-            <p className="title">
-              #StandWithUkraine
-            </p>
+            <div className="content">
+              <div className="columns is-centered">
+                <div className="column is-four-fifths">
+                  <p className="title">
+                    <img src={standWithUkraine} alt="#StandWithUkraine" />
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="hero-foot has-text-centered">
-          hello
+      </section>
+
+      <section className="section is-medium negarive-offset">
+        <div className="content">
+          <div className="columns is-centered">
+            <div className="column is-two-thirds">
+              <div className="box p-6 is-size-4 is-family-monospace">
+                <p>
+                  ПУТИН нам врет! Наше телевидение нам врет!
+                </p>
+                <p>
+                  Мы НЕ проводим специальную операцию по спасению народа Украины. Мы начали полномасштабное вторжение в Украину.
+                  Мы применяем авиацию, наносим ракетные удары по жилым массивам Киева, Харькова, Мариуполя, Волновахи и других городов.
+                  Мы обстреливали Запорожскую атомную электростанцию.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
 
-      <section className="section">
-
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, dignissimos animi cumque alias cupiditate illum quae optio est provident neque, deleniti eum ad soluta delectus molestiae nemo ex explicabo eos?
-      </section>
+      <footer className="footer">
+        <div className="content has-text-centered">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptatum vitae officiis porro dolor ab recusandae dolorem, pariatur delectus molestiae unde nulla quae voluptatibus quas repellendus maxime, sunt temporibus vel?
+        </div>
+      </footer>
     </>
   )
 };
